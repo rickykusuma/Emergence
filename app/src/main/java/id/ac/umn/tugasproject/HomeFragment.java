97ec38c68135a4b37cd41bec85137432c09e8775
rvn9 <andreas.wong@student.umn.ac.id>
2020-04-14 16:34:26 +0700

    Circle Menu
    Circle Menu

--- app/src/main/java/id/ac/umn/tugasproject/HomeFragment.java
+++ app/src/main/java/id/ac/umn/tugasproject/HomeFragment.java
@@ -1,18 +1,46 @@
 package id.ac.umn.tugasproject;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View home_inflater = inflater.inflate(R.layout.fragment_home, container, false);
+        CircleMenu circleMenu = (CircleMenu)home_inflater.findViewById(R.id.menu);
+
+        final String[] menus = {
+          "Pemadam Kebakaran",
+          "Rumsah Sakit",
+          "Kantor Polisi"
+        };
+
+        circleMenu.setMainMenu(Color.parseColor("#CDCDCD"), R.drawable.ic_add,R.drawable.ic_clear)
+                .addSubMenu(Color.parseColor("#00CD00"),R.drawable.ic_home_black_24dp)
+                .addSubMenu(Color.parseColor("#00CD00"),R.drawable.ic_home_black_24dp)
+                .addSubMenu(Color.parseColor("#00CD00"),R.drawable.ic_home_black_24dp)
+                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+                    @Override
+                    public void onMenuSelected(int i) {
+                        Toast.makeText(getActivity(),"YOU CLICKED " + menus[i], Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+        return home_inflater;
     }
 }
