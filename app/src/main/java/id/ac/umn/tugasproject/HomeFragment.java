4e7bef2d829e1e23fbea2f4d2b2ee1b8e00f1188
stevenwijaya2 <steven.wijaya2@student.umn.ac.id>
2020-04-15 01:39:02 +0700

    can sent SMS
    udah bisa kirim sms tapi belom bisa dapet lokasi

--- app/src/main/java/id/ac/umn/tugasproject/HomeFragment.java
+++ app/src/main/java/id/ac/umn/tugasproject/HomeFragment.java
@@ -1,11 +1,17 @@
 package id.ac.umn.tugasproject;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,19 +27,41 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+import java.util.Objects;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
 public class HomeFragment extends Fragment {
 
     private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int MY_PERMISSIONS_REQUEST_ALL_PERMISSION = 1;
     FloatingActionButton menu,polisi,rumahSakit;
     Animation fabOpen, fabClose, rotateFoward, rotateBakcward;
     boolean isOpen = false;
     String noPolisi = "1234";
     String noAmbulance = "12345";
+    String mobilenumber = "1234";
+    String msgbody;
+    Double Latitude,Longitude;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    private LocationCallback locationCallback;
+    private Object mLocationResult;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest locationRequest;
+    private boolean requestingLocationUpdates;
 
     @Nullable
     @Override
@@ -46,6 +74,7 @@ public class HomeFragment extends Fragment {
         fabClose = AnimationUtils.loadAnimation(getActivity(),R.anim.fab_close);
         rotateBakcward =  AnimationUtils.loadAnimation(getActivity(),R.anim.rotate_backward);
         rotateFoward =  AnimationUtils.loadAnimation(getActivity(),R.anim.rotate_foward);
+        ask_permission();
 
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +87,18 @@ public class HomeFragment extends Fragment {
             @Override
             public boolean onLongClick(View v) {
                 Toast.makeText(getActivity(),"MASUK LONG PRESS", LENGTH_SHORT).show();
+                // Permission has already been granted
+                try{
+                    Toast.makeText(getActivity(), "Masuk TRY", Toast.LENGTH_SHORT).show();
+                    msgbody = "ini pesan otomatis, jika anda menerima pesan ini berarti pemilik nomor hp ini sedang dalam bahaya " +
+                            "berikut adalah lokasi pemilik nomor hp ini : ";
+                    SmsManager smgr = SmsManager.getDefault();
+                    smgr.sendTextMessage(mobilenumber,null,msgbody,null,null);
+                    Toast.makeText(getActivity(), "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(getActivity(), "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
+                }
                 return true;
             }
         });
@@ -66,24 +107,9 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 animateFab();
-                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    // Permission is not granted
-                    // Should we show an explanation?
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.CALL_PHONE)) {
-                        // Show an explanation to the user *asynchronously* -- don't block
-                        // this thread waiting for the user's response! After the user
-                        Toast.makeText(getActivity(),"We Need Your Permission to Make a Phone Call to Police Station", LENGTH_SHORT).show();
-                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
-                    } else {
-                        // No explanation needed; request the permission
-                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
-                    }
-                } else {
-                    // Permission has already been granted
-                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + noPolisi));
-                    startActivity(intent);
-                    Toast.makeText(getActivity(), "Calling Police Station", LENGTH_SHORT).show();
-                }
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + noPolisi));
+                startActivity(intent);
+                Toast.makeText(getActivity(), "Calling Police Station", LENGTH_SHORT).show();
             }
         });
 
@@ -91,29 +117,16 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 animateFab();
-                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    // Permission is not granted
-                    // Should we show an explanation?
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.CALL_PHONE)) {
-                        // Show an explanation to the user *asynchronously* -- don't block
-                        // this thread waiting for the user's response! After the user
-                        Toast.makeText(getActivity(),"We Need Your Permission to Make a Phone Call to Ambulance", LENGTH_SHORT).show();
-                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
-                    } else {
-                        // No explanation needed; request the permission
-                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
-                    }
-                } else {
-                    // Permission has already been granted
-                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + noAmbulance));
-                    startActivity(intent);
-                    Toast.makeText(getActivity(), "Calling Ambulance", LENGTH_SHORT).show();
-                }
+                // Permission has already been granted
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + noAmbulance));
+                startActivity(intent);
+                Toast.makeText(getActivity(), "Calling Ambulance", LENGTH_SHORT).show();
             }
         });
 
         return home_inflater;
     }
+
 
     private void animateFab(){
         if(isOpen){
@@ -133,5 +146,31 @@ public class HomeFragment extends Fragment {
             isOpen = true;
         }
     }
+
+    public void ask_permission() {
+        // Ask Permission for first time
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.CALL_PHONE) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.SEND_SMS)) {
+                // sees the explanation, try again to request the permission.
+                Toast.makeText(getActivity(), "We Need Your Permission to Fully Operating", LENGTH_SHORT).show();
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.SEND_SMS, Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ALL_PERMISSION);
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+        // ask permission of location everytime
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ALL_PERMISSION);
+        }
+
+    }
+
 
 }
