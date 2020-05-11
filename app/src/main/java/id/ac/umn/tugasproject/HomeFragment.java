f821003329395b70c18e12ea6fe0afd4aaecdfef
stevenwijaya2 <steven.wijaya2@student.umn.ac.id>
2020-05-12 01:41:55 +0700

    fix logout

--- app/src/main/java/id/ac/umn/tugasproject/HomeFragment.java
+++ app/src/main/java/id/ac/umn/tugasproject/HomeFragment.java
@@ -471,7 +471,7 @@ public class HomeFragment extends Fragment {
         Log.d("SU_FB_STR","Audio Storage Ref : "+mAudioRef);
     }
     private void Update_Location_FireBase(final String mGeoHash){
-
+    if(!(FirebaseAuth.getInstance().getCurrentUser().equals(null))){
         FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                                                     @Override
@@ -488,6 +488,8 @@ public class HomeFragment extends Fragment {
                                                     public void onCancelled(DatabaseError databaseError) {}
                                                 }
                 );
+    }
+
     }
     private void GetNearbyUserPhoneNumber(){
         if(mHashesCurrentLocation != null){
